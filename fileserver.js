6bba8db87d606498ceb14e4090b5eea35f112799
cemedisball <cemedisbod@gmail.com>
2024-06-26 11:31:54 +0700

    first commit

---
+++ fileserver.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const users = require('./db.json');
+const app = express(); 
+const port = process.env.PORT || 3000;
+
+app.get('/api/users', (req, res) => {
+    res.json(users);
+ });
+
+ app.get('/api/users/:id', (req, res) => {
+    res.json(users.find(user => user.id === Number(req.params.id)))
+});
+
+app.get("/", (req, res) => { res.send("Hello! My name is ball");});
+app.listen(port, () => {
+    console.log("Starting node.js at http://127.0.0.1:" + port + "/api/users");
+});
