695c7cdd0fd88dea7b415971c9e72ebeb05968b1
cemedisball <cemedisbod@gmail.com>
2024-07-03 09:49:24 +0700

    first commit

--- fileserver.js
+++ fileserver.js
@@ -1,17 +1,41 @@
 const express = require("express");
 const users = require('./db.json');
-const app = express(); 
-const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3200;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/api/users', (req, res) => {
     res.json(users);
- });
-
- app.get('/api/users/:id', (req, res) => {
-    res.json(users.find(user => user.id === Number(req.params.id)))
 });
 
-app.get("/", (req, res) => { res.send("Hello! My name is ball");});
+app.get('/api/users/:id', (req, res) => {
+
+    res.json(users.find(user => user.id === Number(req.params.id)));
+
+});
+
+app.get("/satang", (req, res) => { res.send("Hello! My name is Ball"); });
+
+app.post('/api/users', (req, res) => {
+    users.push(req.body);
+    let json = req.body;
+    console.log(json);
+    res.send("Username: " + json.username + " inserted.");
+
+});
+
+app.delete('/users/:id', (req, res) => {
+
+    const deletedIndex = users.findIndex(user => user.id === Number(req.params.id)) 
+ 
+    res.send(`Delete user '${users[deletedIndex].username}' completed.`)
+ 
+ })
+
 app.listen(port, () => {
+    console.log("Starting node.js at http://127.0.0.1:" + port);
     console.log("Starting node.js at http://127.0.0.1:" + port + "/api/users");
 });
